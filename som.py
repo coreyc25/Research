f992a969cf922527b1c9bad3a94ecd812a4bb2fc
seandavidsy <seandavidsy@gmail.com>
2023-09-27 18:25:07 -0400

    Changed to SKLearn verison of algorithm and added visualization

--- som.py
+++ som.py
@@ -1,68 +1,40 @@
-#Self organizing map clustering algorithm
-# The Academician
-import csv
-import numpy as np, numpy.random
-from scipy.spatial import distance
-np.set_printoptions(suppress=True) #Force-suppress all exponential notation
+"""
+Example using simple SOM to cluster sklearn's Iris dataset.
 
-""" file = open('mostTraversedLatLong.csv')
-type(file)
-csvreader = csv.reader(file)
+NOTE: This example uses sklearn and matplotlib, but neither is required to use
+the som (sklearn is used here just for the data and matplotlib just to plot
+the output).
 
-header = []
-header = next(csvreader)
-header
+@author: Riley Smith
+Created: 2-1-2021
+"""
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn_som.som import SOM
+from sklearn import datasets
+import pandas as pd
+import numpy as np
 
-rows = []
-for row in csvreader:
-    rows.append(row)
-    rows """
-k = 2
-p = 0
-alpha = 0.7 # Initial learning rate
+# Import data into Pandas DataFrame
+geo_data = pd.read_csv("mostTraversedLatLong.csv", usecols = ['Lat', 'Long'])
+geo_data = np.array(geo_data)
 
-rows = np.array([
-        [1,1,0,0], 
-       [0,1,0,0], 
-       [0,0,1,0], 
-       [0,0,1,1]])
+# SOM algorithm
+clusters = 3
+features = len(geo_data[0])
+som = SOM(m = clusters, dim = features)
 
-#X = np.fromfile
+# Fit algorithm to the data
+som.fit(geo_data)
 
+# Assign each datapoint to its predicted cluster
+predictions = som.predict(geo_data)
 
-# Print the number of data and dimension 
-n = len(rows)
-d = len(rows[0])
-addZeros = np.zeros((n, 1))
-rows = np.append(rows, addZeros, axis=1)
-print("The SOM algorithm: \n")
-print("The training data: \n", rows)
-print("\nTotal number of data: ",n)
-print("Total number of features: ",d)
-print("Total number of Clusters: ",k)
+# Plot the results
+x = geo_data[:,0]
+y = geo_data[:,1]
+colors = ['red', 'green', 'blue']
 
-C = np.zeros((k,d+1))
-
-weight = np.random.rand(n,k)
-print("\nThe initial weight: \n", np.round(weight,2))
-
-for it in range(100): # Total number of iterations
-    for i in range(n):
-        distMin = 99999999
-        for j in range(k):
-            dist = np.square(distance.euclidean(weight[:,j], rows[i,0:d]))
-            if distMin>dist:
-                distMin = dist
-                jMin = j
-        weight[:,jMin] = weight[:,jMin]*(1-alpha) + alpha*rows[i,0:d]   
-    alpha = 0.5*alpha
-    
-print("\nThe final weight: \n",np.round(weight,4))
-
-for i in range(n):    
-    cNumber = np.where(weight[i] == np.amax(weight[i]))
-    rows[i,d] = cNumber[0]
-    
-print("\nThe data with cluster number: \n", rows)
-
-#file.close()
+# Visualization
+plt.scatter(x, y, c=predictions, cmap=ListedColormap(colors))
+plt.show()
