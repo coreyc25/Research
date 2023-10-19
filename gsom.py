c5e7810cc27be4199183c10ce32e2786622827c8
seandavidsy <seandavidsy@gmail.com>
2023-10-18 23:53:05 -0400

    Updated gsom.py with growNode function

--- gsom.py
+++ gsom.py
@@ -16,94 +16,127 @@ import pandas as pd
 import numpy as np
 import math
 
-# Import data into Pandas DataFrame
-geo_data = pd.read_csv("mostTraversedLatLong.csv", usecols = ['Lat', 'Long'])
-geo_data = np.array(geo_data)
-
-# SOM algorithm
-clusters = 3
-n = len(geo_data)
-k = len(geo_data[0])
-som = SOM(m = clusters, dim = k)
-
-# Spread factor
-sf = 1
-
-# neighborhood radius, initially large
-radius = 1
-
-# Growth threshold
-GT = -2 * math.log(sf)
-
-def FindWinner(neurons, p):
-    # Convert the dataset element 'd' into a NumPy array for easy computation
-    p = np.array(p)
+# In terms of implementation, what is the best way to represent a neighborhood of nodes
+class Neurons:
+    def __init__(self, size, features, radius, lr):
+        self.size = size
+        self.count = size * size
+        self.radius = radius
+        self.lr = lr
+        self.weights = np.random.rand(size, size, features)
+        self.iteration = 0
     
-    # Initialize variables to keep track of the minimum error and the corresponding winning neuron
-    min_error = float('inf')
-    winning_neuron = None
-    
-    # Iterate through the neurons in the set and find the neuron with the smallest error
-    for neuron in neurons:
-        # Calculate the Euclidean distance between the dataset element 'd' and the neuron
-        error = np.linalg.norm(neuron - p)
+    """
+    Params:
+        winner: is the winning neuron found from training (had the minimum distance)
+        k: is the current iteration value being called
+    """
+    def adaptWeights(self, winner, p):
+        # Calculate the distance between each neuron and the winning neuron
+        distances = self.weights - p
         
-        # If the error is smaller than the current minimum, update the minimum error and the winning neuron
-        if error < min_error:
-            min_error = error
-            winning_neuron = neuron
+        # Find the neurons within the specified radius
+        neighborhood = []
+        for row_idx in range(len(self.weights)):
+            for col_idx in range(len(self.weights[0])):
+                cur_neuron = self.weights[row_idx][col_idx]
+                distance = np.linalg.norm(winner - cur_neuron)
+                if distance <= self.radius:
+                    neighborhood.append((row_idx, col_idx))
+
+        learning_factor = (self.iteration + 1) / self.count
+        self.lr = math.pow(0.02, learning_factor)
+
+        # Update the weights of neurons within the radius
+        new_delta = 0
+        for row_idx, col_idx in neighborhood:
+            delta = self.lr * distances[row_idx][col_idx]
+            self.weights[row_idx][col_idx] += delta
+            new_delta += abs(delta[0]) + abs(delta[1])
+
+        return new_delta
     
-    return winning_neuron, min_error
+    def growNode(self, p):
+        self.weights = np.append(self.weights, p)
 
-def AdaptWeights(winner, p, radius):
-    new_delta = 0
-    return new_delta
+if __name__ == '__main__':
 
-weights_delta = float('inf')
-tolerance = 0.1
+    # Import data into Pandas DataFrame
+    geo_data = pd.read_csv("mostTraversedLatLong.csv", usecols = ['Lat', 'Long'])
+    geo_data = np.array(geo_data)
 
-D = geo_data
-N = "neurons network"
-neurons = np.random.rand(n,k)
-distances = []
+    neuron_size = 10
+    features = 2
+    neighborhood_radius = 2
+    learning_rate = 0.02
+    neurons = Neurons(neuron_size, features, neighborhood_radius, learning_rate)
 
-while weights_delta > tolerance:
-    weights_delta = 0
-    for p in D:
-        winner, error = FindWinner(N, p)
-        weights_delta = AdaptWeights(winner, p, radius)
-        if error >= GT:
-            GrowNode(N, p)
-        N.iteration += 1
+    # Spread factor
+    sf = 1
 
+    # Growth threshold
+    GT = -2 * math.log(sf)
 
+    def FindWinner(neurons, p):
+        # Convert the dataset element 'd' into a NumPy array for easy computation
+        p = np.array(p)
+        
+        # Initialize variables to keep track of the minimum error and the corresponding winning neuron
+        min_error = float('inf')
+        winning_neuron = None
+        
+        # Iterate through the neurons in the set and find the neuron with the smallest error
+        for neuron in neurons:
+            # Calculate the Euclidean distance between the dataset element 'd' and the neuron
+            error = np.linalg.norm(neuron - p)
+            
+            # If the error is smaller than the current minimum, update the minimum error and the winning neuron
+            if error < min_error:
+                min_error = error
+                winning_neuron = neuron
+        
+        return winning_neuron, min_error
 
-# Algorithm 2 Smoothing
-r_large = r * 2
-for i in range(50):
-    for p in D:
-        winner, error = FindWinner(N, p)
-        AdaptWeights(winner, p, r_large)
+    # Algorithm 1 Learn and Grow
+    weights_delta = float('inf')
+    tolerance = 0.5
 
-r_small = r * 0.5
-for i in range(50):
-    for p in D:
-        winner, error = FindWinner(N, p)
-        AdaptWeights(winner, p, r_small)
+    while weights_delta > tolerance:
+        weights_delta = 0
+        for p in geo_data:
+            winner, error = FindWinner(neurons.weights, p)
+            weights_delta = neurons.adaptWeights(winner, p)
+            if error >= GT:
+                neurons.growNode(p)
+            neurons.iteration += 1
 
-# Everything below here is for visualization
+    # Algorithm 2 Smoothing
+    neurons.radius = neighborhood_radius * 2
+    for i in range(50):
+        for p in geo_data:
+            winner, error = FindWinner(neurons.weights, p)
+            neurons.adaptWeights(winner, p)
 
-# Fit algorithm to the data
-som.fit(geo_data)
+    neurons.radius = neighborhood_radius * 0.5
+    for i in range(50):
+        for p in geo_data:
+            winner, error = FindWinner(neurons.weights, p)
+            neurons.adaptWeights(winner, p)
 
-# Assign each datapoint to its predicted cluster
-predictions = som.predict(geo_data)
+    print(neurons.weights)
+    # # Everything below here is for visualization
 
-# Plot the results
-x = geo_data[:,0]
-y = geo_data[:,1]
-colors = ['red', 'green', 'blue']
+    # # Fit algorithm to the data
+    # som.fit(geo_data)
 
-# Visualization
-plt.scatter(x, y, c=predictions, cmap=ListedColormap(colors))
-plt.show()
+    # # Assign each datapoint to its predicted cluster
+    # predictions = som.predict(geo_data)
+
+    # # Plot the results
+    # x = geo_data[:,0]
+    # y = geo_data[:,1]
+    # colors = ['red', 'green', 'blue']
+
+    # # Visualization
+    # plt.scatter(x, y, c=predictions, cmap=ListedColormap(colors))
+    # plt.show()
